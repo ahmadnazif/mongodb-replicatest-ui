ccbf21f91189dd64680d5837d5e6f5042be85800
Nazif Abdullah <ahmadnazif@gmail.com>
2023-09-27 15:32:25 +0800

    Add multiple packages

---
+++ MongoDbReplicaLoadTest/Client/HubClients/SmsClient.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+namespace MongoDbReplicaLoadTest.Client.HubClients;
+
+public class SmsClient : IAsyncDisposable
+{
+    private const string HUB_ROUTE = "/sms-hub";
+    private string clientId = "BlazorClient";
+    private readonly ILogger<SmsClient> logger;
+    private readonly HubConnection hubConnection;
+    private CancellationTokenSource cts;
+    public async Task StopAsync()
+    {
+        try
+        {
+            await hubConnection.StopAsync();
+            IsConnected = false;
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex.Message);
+        }
+    }
+    public async ValueTask DisposeAsync()
+    {
+        await StopAsync();
+        GC.SuppressFinalize(this);
+    }
+}
